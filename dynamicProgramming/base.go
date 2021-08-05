7f6410102327264fa38a139f3227626c0bf069ae
liangxiangtao <liangxiangtao@xinyu668.com>
2021-08-05 22:36:05 +0800

    dp

--- dynamicProgramming/base.go
+++ dynamicProgramming/base.go
@@ -3,3 +3,35 @@
 // @Describe :  
 
 package dynamicProgramming
+
+var SolutionMap map[int]int
+
+func init()  {
+	SolutionMap = make(map[int]int)
+}
+
+
+func Solution(n int32) int {
+	if n == 1 {
+		return 1
+	}
+
+	if n == 2 {
+		return 2
+	}
+
+	return Solution(n - 1) + Solution(n - 2)
+}
+
+
+func Solution2(n int32) int {
+	if n == 1 {
+		return 1
+	}
+
+	if n == 2 {
+		return 2
+	}
+
+	return 0
+}
---
+++ dynamicProgramming/base_test.go
@@ -0,0 +1,17 @@
+// @Author : a1234
+// @Time : 2021/8/5 10:25 下午 
+// @Describe :  
+
+package dynamicProgramming
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	var n int32 = 10
+	res := Solution(n)
+	fmt.Println("res:", res)
+
+}
