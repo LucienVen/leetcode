891910573c33a94a72f7d76bf2ee7aa67c97663a
liangxiangtao <liangliang.too@foxmail.com>
2025-05-23 15:04:18 +0800

    no56. 合并区间, done

--- simpleArray/no56.go
+++ simpleArray/no56.go
@@ -1,1 +1,80 @@
 package simpleArray
+
+import (
+	"sort"
+)
+
+// 56. 合并区间
+// https://leetcode.cn/problems/merge-intervals/description/
+
+func Merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return intervals
+	}
+
+	// 排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	for i := 0; i < len(intervals); i++ {
+		handle := intervals[i]
+
+		for j := i + 1; j < len(intervals); j++ {
+			target := intervals[j]
+
+			merge := max(handle[0], target[0]) <= min(handle[1], target[1])
+			if merge {
+				// 区间重叠
+				// 移除元素
+				intervals = append(intervals[:j], intervals[j+1:]...)
+				intervals = append(intervals[:i], intervals[i+1:]...)
+
+				intervals = append(intervals, []int{min(handle[0], target[0]), max(target[1], handle[1])})
+
+				i = -1 // 重新再来
+				break
+			}
+		}
+	}
+
+	// 排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	return intervals
+}
+
+func Merge2(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return intervals
+	}
+
+	// 排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	start, end := intervals[0][0], intervals[0][1]
+	ans := make([][]int, 0)
+
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] > end {
+			// 新的元素，左端点大于当前的右端点，则表示不连续
+
+			// 旧元素存入结果
+			ans = append(ans, []int{start, end})
+			// 新区间重新开始
+			start, end = intervals[i][0], intervals[i][1]
+		} else {
+			// 重叠
+			end = max(end, intervals[i][1])
+		}
+	}
+
+	// 最后区间存入
+	ans = append(ans, []int{start, end})
+
+	return ans
+}
--- simpleArray/no56_test.go
+++ simpleArray/no56_test.go
@@ -1,1 +1,101 @@
 package simpleArray
+
+import "testing"
+
+func TestMerge(t *testing.T) {
+
+	cases := []struct {
+		Name     string
+		Input    [][]int
+		Expected [][]int
+	}{
+		{
+			Name:     "test1",
+			Input:    [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			Expected: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			Name:     "test2",
+			Input:    [][]int{{1, 4}, {2, 3}},
+			Expected: [][]int{{1, 4}},
+		},
+		{
+			Name:     "test3",
+			Input:    [][]int{{1, 4}, {0, 2}, {3, 5}},
+			Expected: [][]int{{0, 5}},
+		},
+		{
+			Name:     "test4",
+			Input:    [][]int{{1, 4}, {2, 3}, {5, 7}, {6, 8}},
+			Expected: [][]int{{1, 4}, {5, 8}},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			result := Merge(c.Input)
+			if len(result) != len(c.Expected) {
+				t.Log("Input: ", c.Input)
+				t.Errorf("expected %v, got %v", c.Expected, result)
+				return
+			}
+			for i := range result {
+				if result[i][0] != c.Expected[i][0] || result[i][1] != c.Expected[i][1] {
+					t.Log("Input: ", c.Input)
+					t.Errorf("expected %v, got %v", c.Expected, result)
+					break
+				}
+			}
+		})
+	}
+
+}
+
+func TestMerge2(t *testing.T) {
+
+	cases := []struct {
+		Name     string
+		Input    [][]int
+		Expected [][]int
+	}{
+		{
+			Name:     "test1",
+			Input:    [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			Expected: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			Name:     "test2",
+			Input:    [][]int{{1, 4}, {2, 3}},
+			Expected: [][]int{{1, 4}},
+		},
+		{
+			Name:     "test3",
+			Input:    [][]int{{1, 4}, {0, 2}, {3, 5}},
+			Expected: [][]int{{0, 5}},
+		},
+		{
+			Name:     "test4",
+			Input:    [][]int{{1, 4}, {2, 3}, {5, 7}, {6, 8}},
+			Expected: [][]int{{1, 4}, {5, 8}},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			result := Merge2(c.Input)
+			if len(result) != len(c.Expected) {
+				t.Log("Input: ", c.Input)
+				t.Errorf("expected %v, got %v", c.Expected, result)
+				return
+			}
+			for i := range result {
+				if result[i][0] != c.Expected[i][0] || result[i][1] != c.Expected[i][1] {
+					t.Log("Input: ", c.Input)
+					t.Errorf("expected %v, got %v", c.Expected, result)
+					break
+				}
+			}
+		})
+	}
+
+}
